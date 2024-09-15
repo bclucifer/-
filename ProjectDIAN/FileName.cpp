6f7347ced6d3ea0ee84ee78ab58caba1b92fbc2e
邓百川 <@LAPTOP-72SQCLK2>
2024-09-15 22:53:57 +0800

    添加项目文件。

---
+++ ProjectDIAN/FileName.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	double price[5] = { 0 };
+char name[6] = { "0" };
+int channl=0;
+int quantity[5] = { 0 };
+int i = 0;
+for (i = 0; i <= 5; i++) {
+	scanf_s("%d", &channl);
+	scanf_s("%c", &name[channl-1]);
+	scanf_s("%lf", &price[channl-1]);
+	scanf_s("%d", &quantity[channl-1]);
+	printf("货物%c摆放在%d通道，单价为%lf，数量为%d个", name[channl-1], channl, price[channl-1], quantity[channl-1]);
+}
+return 0;
+}
